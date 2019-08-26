1733371e47661fe2cc80176a339306ab9a660a1b
zhangxiaocheng <zhangxiaocheng@aegis-data.cn>
2019-08-26 15:34:35 +0800

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='casearch',
-    version='0.0.1',
+    version='0.0.2',
     author='TnTomato',
     author_email='474093103@qq.com',
     description='Retrieve from ElasticSearch',
